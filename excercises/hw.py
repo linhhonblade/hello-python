53f2331605cde433f673ce49cebef28c636abace
maiptn <mai.pham@on.net.vn>
2021-10-11 16:15:15 +0700

    more exercises

--- excercises/hw.py
+++ excercises/hw.py
@@ -46,3 +46,28 @@
 
 # 7. ==============================================
 # Implement one of these sorting algorithms: bubble sort, quick sort, insertion sort, selection sort, merge sort
+
+# 8. ==============================================
+# Define a class which has at least two methods:
+# getString: to get a string from console input
+# printString: to print the string in upper case.
+# Also please include simple test function to test the class methods.
+
+# 9. ==============================================
+# You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, age and height are numbers. The tuples are input by console. The sort criteria is:
+# 1: Sort based on name;
+# 2: Then sort based on age;
+# 3: Then sort by score.
+# The priority is that name > age > score.
+# If the following tuples are given as input to the program:
+# Tom,19,80
+# John,20,90
+# Jony,17,91
+# Jony,17,93
+# Json,21,85
+# Then, the output of the program should be:
+# [('John', '20', '90'), ('Jony', '17', '91'), ('Jony', '17', '93'), ('Json', '21', '85'), ('Tom', '19', '80')]
+
+# 10. ==============================================
+# Define a class named Rectangle which can be constructed by a length and width. 
+# The Rectangle class has a method which can compute the area. 
