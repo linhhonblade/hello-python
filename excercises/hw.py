c9a8577c4615fc4dd02efb7b872eacd3f8e62055
maiptn <mai.pham@on.net.vn>
2021-10-11 16:08:37 +0700

    first commit

---
+++ excercises/hw.py
@@ -0,0 +1,48 @@
+# 1. =============================================
+# Write a program (function!) that takes a list
+# and returns a new list that contains all the elements
+# of the first list minus all the duplicates.
+
+# 2. =============================================
+# Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
+
+# 3. =============================================
+# Ask the user for a number and determine whether the number is prime or not.
+# (For those who have forgotten, a prime number is a number that has no divisors.)
+
+# 4. =============================================
+# a = {1: 'misa', 11: 'hello', 3: 23}
+# Find the way to:
+# show all the keys in a
+# return value of key 11
+# clear the entire dictionary a
+# get the length of a
+# get the type of a
+# identify the key with the highest value (among other keys)
+# identify the key with the lowest value (among other keys)
+
+# 5. ==============================================
+# b = list(range(10))
+# Guest the result of the following statement and check them yourself by python
+# print(b[1:3])
+# print(b[2:6])
+# print(b[2:6:2])
+# print(b[0:8:3])
+# print(b[::2])
+# print(b[::])
+# print(b[:5:2])
+# print(b[3::2])
+# print(b[:-2])
+# print(b[2:-2])
+# print(b[::-1])
+# print(b[:-2:-2])
+
+# 6. ==============================================
+# c = [1, 12, 43, 44, 64, 45, 90, 6, 27, 231, 432, 88]
+# Use sorted() to sort c in ascendant order
+# Use filter() to filter only values greater than 50 from c
+# Use map() to double every item in c
+# Use reduce() to calculate sum of all items in c (from functools module)
+
+# 7. ==============================================
+# Implement one of these sorting algorithms: bubble sort, quick sort, insertion sort, selection sort, merge sort
