f4bfd8556ad7db5b2067a1665cf935c98d14554d
maiptn <mai.pham@on.net.vn>
2021-10-12 09:40:19 +0700

    update

--- excercises/hw-day1.py
+++ excercises/hw-day1.py
@@ -1,16 +1,11 @@
 # 1. =============================================
-# Write a program (function!) that takes a list
-# and returns a new list that contains all the elements
-# of the first list minus all the duplicates.
-
-# 2. =============================================
 # Write a program that asks the user how many Fibonnaci numbers to generate and then generates them.
 
-# 3. =============================================
+# 2. =============================================
 # Ask the user for a number and determine whether the number is prime or not.
 # (For those who have forgotten, a prime number is a number that has no divisors.)
 
-# 4. =============================================
+# 3. =============================================
 # a = {1: 'misa', 11: 'hello', 3: 23}
 # Find the way to:
 # show all the keys in a
@@ -21,7 +16,7 @@
 # identify the key with the highest value (among other keys)
 # identify the key with the lowest value (among other keys)
 
-# 5. ==============================================
+# 4. ==============================================
 # b = list(range(10))
 # Guest the result of the following statement and check them yourself by python
 # print(b[1:3])
@@ -36,6 +31,34 @@
 # print(b[2:-2])
 # print(b[::-1])
 # print(b[:-2:-2])
+
+# 5. ==============================================
+# Couting sheep program
+# Create a program to count sheep
+# Start with zero sheep
+# if user input 'n', add one sheep
+# if user input 'p', minus one sheep
+# if user input 'c', print the number of sheeps counted
+# if number of sheep is 0 and user input 'q', send a message 'No sheep left'
+# if user enter 'q', print the number of sheeps counted and end the program
+# if user enter any other character: do nothing
+
+
+
+
+
+
+
+
+
+# 1. =============================================
+# Write a program (function!) that takes a list
+# and returns a new list that contains all the elements
+# of the first list minus all the duplicates.
+
+
+
+
 
 # 6. ==============================================
 # c = [1, 12, 43, 44, 64, 45, 90, 6, 27, 231, 432, 88]
@@ -54,7 +77,9 @@
 # Also please include simple test function to test the class methods.
 
 # 9. ==============================================
-# You are required to write a program to sort the (name, age, height) tuples by ascending order where name is string, age and height are numbers. The tuples are input by console. The sort criteria is:
+# You are required to write a program to sort the (name, age, height) tuples by ascending order 
+# where name is string, age and height are numbers. 
+# The tuples are input by console. The sort criteria is:
 # 1: Sort based on name;
 # 2: Then sort based on age;
 # 3: Then sort by score.
